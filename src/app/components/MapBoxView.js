2baed12fa5097a2acff1eabf386a0d8f06c638c3
Rafael Dalprá <thedalpra@gmail.com>
2020-10-29 11:30:13 +0000

    Adds switch to open labels by default or not

--- src/app/components/MapBoxView.js
+++ src/app/components/MapBoxView.js
@@ -5,6 +5,8 @@ import { renderToString } from "react-dom/server";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
+const openLabels = true;
+
 const MapBoxView = ({ markers }) => {
   const classes = useStyles();
   const mapContainerRef = useRef(null);
@@ -27,7 +29,9 @@ const MapBoxView = ({ markers }) => {
           .setPopup(new mapboxgl.Popup().setHTML(renderToString(item)))
           .setLngLat([item.props.lng, item.props.lat])
           .addTo(map);
-        marker.togglePopup();
+        if (openLabels) {
+          marker.togglePopup();
+        }
       });
     });
 
